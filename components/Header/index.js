cf214b30f4c2c8484d7f12824141be30f1aad033
Julián Prieto <herbie.juls@gmail.com>
2021-01-01 20:06:06 -0300

    Fix header overflow

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,29 +4,33 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="pt-10 flex justify-center items-center text-xs font-medium tracking-widest z-10">
-      <Link href="/hola">
-        <a className="mr-8">¡HOLA!</a>
-      </Link>
-      <Link href="/portfolio">
-        <a className="mr-8">PORTFOLIO</a>
-      </Link>
-      <Link href="/clases">
-        <a className="mr-8">CLASES</a>
-      </Link>
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <Link href="/hola">
+          <a className="md:mr-8">¡HOLA!</a>
+        </Link>
+        <Link href="/portfolio">
+          <a className="mt-3 md:mt-0 md:mr-8">PORTFOLIO</a>
+        </Link>
+        <Link href="/clases">
+          <a className="mt-3 md:mt-0 md:mr-8">CLASES</a>
+        </Link>
+      </div>
       <Link href="/">
-        <a className="mr-8">
+        <a className="mx-8 md:mx-0">
           <Image src="/logo.svg" alt="Logo" width={150} height={150} />
         </a>
       </Link>
-      <Link href="/prensa">
-        <a className="mr-8">PRENSA</a>
-      </Link>
-      <Link href="/tienda">
-        <a className="mr-8">TIENDA</a>
-      </Link>
-      <Link href="/contacto">
-        <a className="mr-8">CONTACTO</a>
-      </Link>
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <Link href="/prensa">
+          <a className="md:ml-8">PRENSA</a>
+        </Link>
+        <Link href="/tienda">
+          <a className="mt-3 md:mt-0 md:ml-8">TIENDA</a>
+        </Link>
+        <Link href="/contacto">
+          <a className="mt-3 md:mt-0 md:ml-8">CONTACTO</a>
+        </Link>
+      </div>
     </div>
   );
 }
