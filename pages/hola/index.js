b954bd1d449b309533627df1ba721203687a94d7
Julián Prieto <julian.prieto@ey.com>
2021-01-04 19:21:45 -0300

    Add needle to text

--- pages/hola/index.js
+++ pages/hola/index.js
@@ -37,40 +37,27 @@ export default function Hola() {
                   </div>
                 </p>
                 <p className="mb-4 text-lg leading-tight">
-                  licenciada en diseño gráfico y fanática del bordado
-                  "punch-needle".
+                  licenciada en diseño gráfico y bordadora argentina de punch
+                  needle.
                 </p>
                 <p className="mb-4 text-lg leading-tight">
-                  Me topé con esta técnica de bordado de casualidad, ante el
-                  deseo de decorar mi hogar con objetos textiles que existían en
-                  mi cabeza pero no en el mercado. De forma autodidacta, después
-                  de muchos intentos, aprendí a domar la "aguja mágica" y logré
-                  lucir con orgullo los almohadones que soñaba en mi casa.
-                </p>
-                <p className="mb-4 text-lg leading-tight">
-                  Ese fue solo el inicio de un gran camino de experimentación
-                  con telas, lanas y muchas agujas de formas raras que, con mis
+                  De forma autodidacta aprendí a domar la "aguja mágica" y ese
+                  fue el inicio de un gran camino de experimentación con telas,
+                  lanas y muchas agujas de formas raras que, con mis
                   conocimientos de diseño, fotografía e ilustración, me llevó a
-                  crear un gran portfolio de almohadones con diseños propios y
-                  personajes de historias únicas.
-                </p>
-                <p className="mb-4 text-lg leading-tight">
-                  Mi proceso creativo empieza en el universo digital y termina
-                  con lo más analógico del mundo: mis manos. Me gusta el
-                  contraste que genera diseñar motivos vectoriales en la
-                  computadora y verlos finalmente plasmados en lana y tela.
-                  Disfruto comparar las imágenes del plan y el resultado en cada
-                  proyecto.
+                  crear un gran portfolio de almohadones, cuadros y muñecos,
+                  donde las texturas son las protagonistas.
                 </p>
                 <p className="mb-4 text-lg leading-tight">
                   Actualmente reparto mi tiempo entre el diseño gráfico
-                  freelance y este hermoso proyecto en el que me reto a que cada
-                  bordado sea un nuevo desafío. También vendo mis piezas
-                  bordadas a clientes de todo el mundo y doy clases de
-                  punch-needle en mi pequeño hogar-estudio lleno de almohadones.
-                  Algunas de mis piezas fueron publicadas en revistas como
-                  OHLALÁ, varios blogs de decoración y estuvieron presentes en
-                  la edición 2019 de Casa FOA.
+                  freelance y este hermoso proyecto artístico en el que me reto
+                  a que cada bordado sea un nuevo desafío.
+                </p>
+                <p className="mb-4 text-lg leading-tight">
+                  ¡También comparto mis conocimientos! Doy clases presenciales
+                  grupales en mi casa y, en 2020 publiqué mi primer curso online
+                  titulado Bordado XL con Aguja Mágica, dictado en la plataforma
+                  web Doméstika.
                 </p>
                 <div className="relative md:hidden">
                   <Image
