bd2cf5cc51d698233e8a5069bf1e8684ab478d9c
Julián Prieto <julian.prieto@ey.com>
2021-01-04 12:08:00 -0300

    Fix footer spacing

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,37 +3,37 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="flex flex-1 flex-col">
-      <div className="flex flex-1 justify-center items-center mt-32 py-8 px-16 bg-black font-regular text-sm">
+      <div className="flex flex-1 flex-col md:flex-row justify-center items-center mt-32 py-8 px-16 bg-black font-regular text-sm">
         <a
-          className="text-white hover:bg-white hover:text-black py-2 px-4 mr-4"
+          className="text-white hover:bg-white hover:text-black py-2 px-4 md:mr-4"
           href="https://www.instagram.com/porcarobello"
           about="_blank"
         >
           INSTAGRAM
         </a>
         <a
-          className="text-white hover:bg-white hover:text-black py-2 px-4 mr-4"
+          className="text-white hover:bg-white hover:text-black py-2 px-4 md:mr-4"
           href="https://www.facebook.com/porcarobello"
           about="_blank"
         >
           FACEBOOK
         </a>
         <a
-          className="text-white hover:bg-white hover:text-black py-2 px-4 mr-4"
+          className="text-white hover:bg-white hover:text-black py-2 px-4 md:mr-4"
           href="https://www.youtube.com/channel/UCZCBCksy3XIiNj4LA0I17dQ"
           about="_blank"
         >
           YOUTUBE
         </a>
         <a
-          className="text-white hover:bg-white hover:text-black py-2 px-4 mr-4"
+          className="text-white hover:bg-white hover:text-black py-2 px-4 md:mr-4"
           href="https://www.domestika.org/es/carobello"
           about="_blank"
         >
           DOMESTIKA
         </a>
         <a
-          className="text-white hover:bg-white hover:text-black py-2 px-4 mr-4"
+          className="text-white hover:bg-white hover:text-black py-2 px-4 md:mr-4"
           href="https://www.tiktok.com/@porcarobello?lang=es"
           about="_blank"
         >
@@ -47,10 +47,13 @@ export default function Footer() {
           BEHANCE
         </a>
       </div>
-      <div className="flex flex-1 justify-center items-center my-8">
+      <div className="flex flex-1 flex-col md:flex-row justify-center items-center my-8 px-8">
         <Image src="/logo_light.svg" width={24} height={24} alt="Caro Bello" />
-        <span className="text-black font-regular text-sm ml-4">
-          Copyright © 2021 Caro Bello. Todos los derechos reservados.
+        <span className="text-black font-regular text-sm mt-4 mb-2 md:my-0 md:mx-2">
+          Copyright © 2021 Caro Bello.
+        </span>
+        <span className="text-black font-regular text-sm">
+          Todos los derechos reservados.
         </span>
       </div>
     </footer>
--- components/YoutubeVideo/index.js
+++ components/YoutubeVideo/index.js
@@ -14,7 +14,6 @@ export default function YoutubeVideo({ youtubeId }) {
           width: "100%",
           height: "100%",
         }}
-        // src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&loop=1&rel=0`}
         src={`https://www.youtube.com/embed/${youtubeId}?&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=${youtubeId}`}
         frameBorder="0"
         allowfullscreen
