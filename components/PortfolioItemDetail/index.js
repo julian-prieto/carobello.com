fd88e5dbac750f7fb77da3dee27b72fae68f21c8
Julián Prieto <herbie.juls@gmail.com>
2021-01-27 20:19:42 -0300

    Add classes page

--- components/PortfolioItemDetail/index.js
+++ components/PortfolioItemDetail/index.js
@@ -90,9 +90,9 @@ export default function PortfolioItemDetail({ workId, onClose: handleClose }) {
             <path d="M14.989,9.491L6.071,0.537C5.78,0.246,5.308,0.244,5.017,0.535c-0.294,0.29-0.294,0.763-0.003,1.054l8.394,8.428L5.014,18.41c-0.291,0.291-0.291,0.763,0,1.054c0.146,0.146,0.335,0.218,0.527,0.218c0.19,0,0.382-0.073,0.527-0.218l8.918-8.919C15.277,10.254,15.277,9.784,14.989,9.491z"></path>
           </svg>
         </button>
-        <div className="flex flex-col justify-between flex-1 p-4 h-100vh">
-          <div className="my-8">
-            <span className="text-xl md:text-xl xl:text-4xl leading-none">{title}</span>
+        <div className="flex flex-1 flex-col justify-between p-4 h-100vh">
+          <div className="flex my-8">
+            <span className="text-xl md:text-xl xl:text-4xl leading-none whitespace-nowrap">{title}</span>
             <span className="text-xl ml-12 md:text-xl xl:text-xl leading-none">{description}</span>
           </div>
           <div className="flex-1">
