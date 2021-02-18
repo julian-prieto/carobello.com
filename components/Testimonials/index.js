723e3966436882df652f8fcddebcaf21e5a2cfc6
Julián Prieto <herbie.juls@gmail.com>
2021-02-18 14:24:35 -0300

    Add testimonials

--- components/Testimonials/index.js
+++ components/Testimonials/index.js
@@ -12,14 +12,12 @@ export default function Testimonials({ testimonials }) {
     return null;
   }
 
-  // bulletActiveClass: styles["active-testimonial"],
-
   return (
     <Swiper slidesPerView={1} pagination={{ clickable: true, bulletActiveClass: styles["active-testimonial"] }} loop>
       {testimonials.map(({ id, testimonial, author }) => (
         <SwiperSlide key={id}>
-          <div className="flex flex-col flex-1 p-8 items-stretch cursor-move">
-            <div className="self-center md:self-start">
+          <div className="flex flex-col md:flex-row flex-1 p-8 items-stretch cursor-move">
+            <div className="self-center">
               <Image src="/quote_open.svg" alt="Open quote" width={75} height={75} />
             </div>
             <div className="flex flex-col flex-1 items-stretch">
@@ -28,7 +26,7 @@ export default function Testimonials({ testimonials }) {
                 <span className="italic text-sm">{author}</span>
               </div>
             </div>
-            <div className="self-center mt-2 md:mt-0 md:self-end">
+            <div className="self-center mt-2 md:mt-0">
               <Image src="/quote_close.svg" alt="Close quote" width={75} height={75} />
             </div>
           </div>
