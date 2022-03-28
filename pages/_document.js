ed62bbc3031081f6220b98fc6b1d111f0df1f27b
Julián Prieto <herbie.juls@gmail.com>
2022-03-28 19:19:38 -0300

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+const isProd = process.env.NODE_ENV !== "production";
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -11,6 +13,19 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;800&display=swap"
             rel="stylesheet"
           ></link>
+          {isProd && <script async src="https://www.googletagmanager.com/gtag/js?id=G-PJ9WKGJEBR"></script>}
+          {isProd && (
+            <script
+              async
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+             
+               gtag('config', 'G-PJ9WKGJEBR');`,
+              }}
+            />
+          )}
         </Head>
         <body>
           <Main />
