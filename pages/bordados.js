e50f263fc077e8b0eb0d9dc84fd782febe89014d
Julián Prieto <herbie.juls@gmail.com>
2023-04-29 11:19:01 -0300

    Fix /bordados

--- pages/bordados.js
+++ pages/bordados.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import axios from "axios";
-import { generateImageSizes } from "utils";
 import Head from "next/head";
 
 export default function Bordados({ posts }) {
@@ -10,7 +9,7 @@ export default function Bordados({ posts }) {
         <title>Bordados - Caro Bello</title>
       </Head>
       <main className="py-20 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
-        {posts.map(({ id, thumbnail_url, media_url, permalink, caption }) => {
+        {posts.map(({ id, thumbnail_url, media_url, permalink }) => {
           const imageUrl = `/api/image-proxy?imageUrl=${encodeURIComponent(thumbnail_url || media_url)}`;
           return (
             <a
@@ -20,18 +19,7 @@ export default function Bordados({ posts }) {
               target="_blank"
               rel="noreferrer"
             >
-              <Image
-                src={imageUrl}
-                alt={caption}
-                layout="fill"
-                objectFit="cover"
-                // sizes={generateImageSizes({
-                //   sm: "50vw",
-                //   md: "25vw",
-                //   big: "16.66vw",
-                // })}
-                unoptimized
-              />
+              <Image src={imageUrl} alt={permalink} layout="fill" objectFit="cover" unoptimized />
             </a>
           );
         })}
@@ -47,10 +35,16 @@ export async function getStaticProps() {
 
   const url = `https://graph.instagram.com/me/media?limit=100&fields=id,media_type,caption,permalink,thumbnail_url,media_url&access_token=${igToken}`;
   const feed = await axios.get(url);
+  const posts = feed.data.data.map((item) => ({
+    id: item.id || null,
+    thumbnail_url: item.thumbnail_url || null,
+    media_url: item.media_url || null,
+    permalink: item.permalink || null,
+  }));
 
   return {
     props: {
-      posts: feed.data.data,
+      posts,
     },
     revalidate: 60,
   };
